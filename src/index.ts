780650cc8ab2e8592780a4d39b52705eb214834e
Abimael Lovera <abimaellovera@gmail.com>
2022-11-15 18:45:53 -0300

    Criando metodos para Category

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { router } from './route';
 
 
 mongoose.connect('mongodb://localhost:27017')
@@ -7,6 +8,11 @@ mongoose.connect('mongodb://localhost:27017')
     const app = express();
 
     const port = 3001;
+
+    // importante declarara que estamos utilizando json porque o http é um protocolo de hipertexto e temos que fazer o parse do nosso objeto json
+    app.use(express.json());
+
+    app.use(router);
     app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
     });
