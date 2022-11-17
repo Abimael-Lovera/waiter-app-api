65f8953fb1dd19aca9c6a0c55199b582835a7675
Abimael Lovera <abimaellovera@gmail.com>
2022-11-17 20:56:14 -0300

    Atualizando router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { router } from './route';
+import { router } from './router';
 
 
 mongoose.connect('mongodb://localhost:27017')
